c437ff8979ec207c4e69ee927cf5069ed7cbf3ab
Swagata Prateek <thehoneymad@hotmail.com>
2024-09-07 22:20:45 -0700

    Added a small command line tool interface to get started
    I am basically doing rust for a CLI project at the first time.
    I added a small command line tool interface to get ourselves started.
    
    test: cargo build; cargo run;

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
+use clap::{arg, command, CommandFactory, Parser};
+
+#[derive(Parser)]
+#[command(name = "transit-bd")]
+#[command(
+    about = "Utility command line too work with transit data of Bangladesh",
+    version = "1.0"
+)]
+struct Cli {
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    verbose: u8,
+}
+
 fn main() {
-    let _str = "Hello World!";
-    println!("{}", _str);
+    let args = Cli::parse();
+    if std::env::args().len() == 1 {
+        Cli::command().print_help().unwrap();
+        std::process::exit(0);
+    }
+
+    if args.verbose > 0 {
+        println!("Verbose mode enabled.");
+    }
 }
